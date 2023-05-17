4f2790b57149504fc23d84502876f34962cd6d33
bjoernQ <bjoern.quentin@mobile-j.de>
2023-05-17 16:56:52 +0200

    Fix

--- logging.c
+++ logging.c
@@ -7,5 +7,5 @@
 void write_log(uint32_t level, const char *tag, const char *log_line, ...);
 
 extern void somethingsomething() {
-    write_log(42, "TAG", "FORMAT", 1);
+    write_log(42, "TAG", "FORMAT", 1, 2, 3, 4, 5, 6, 7, 8, 9);
 }
